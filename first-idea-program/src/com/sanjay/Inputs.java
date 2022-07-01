a1c1da52da88ef1f2b8765bb486fcf67141ce915
ChandrakantPal <sanjay161097@gmail.com>
2022-07-02 02:06:01 +0530

    enter input

--- first-idea-program/src/com/sanjay/Inputs.java
+++ first-idea-program/src/com/sanjay/Inputs.java
@@ -5,5 +5,6 @@ import java.util.Scanner;
 public class Inputs {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Please enter some input: ");
     }
 }
