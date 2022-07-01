3ae9d07479d27ae91c1df166d5a252a27dc9abf6
ChandrakantPal <sanjay161097@gmail.com>
2022-07-02 03:16:48 +0530

    comment added

--- first-idea-program/src/com/sanjay/TypeCasting.java
+++ first-idea-program/src/com/sanjay/TypeCasting.java
@@ -25,7 +25,19 @@ public class TypeCasting {
 //        byte b = 50;
 //        b = b*2;  this throws an error because while evaluating the expression it is converted to an int which cannot be reassigned to byte b
 
-        int number = 'A';
-        System.out.println(number); // results 65
+//        int number = 'A';
+//        System.out.println(number); // results 65
+//        System.out.println(3*5.9);//results into float
+
+        byte b = 42;
+        char c = 'a';
+        short s = 1024;
+        int i = 50000;
+        float f = 5.67f;
+        double d = 0.1234;
+        double result = (f * b) + (i / c) - (d * s);
+        // float + int - double = double
+        System.out.println((f * b) + " " + (i / c) + " " + (d * s)); // b is promoted to f | c is promoted to i | s is promoted to d | and result is promoted to d
+        System.out.println(result);
     }
 }
