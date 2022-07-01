58730a65f8b42607da41490d1f40adfc13a0a03a
ChandrakantPal <sanjay161097@gmail.com>
2022-07-01 23:23:09 +0530

    char

--- first-idea-program/src/com/sanjay/Primitives.java
+++ first-idea-program/src/com/sanjay/Primitives.java
@@ -3,9 +3,6 @@ package com.sanjay;
 public class Primitives {
     public static void main(String[] args) {
         int rollno = 64;
-
-        // String is not primitive;
-        //primitive  data type is something  cannot be breakdown further
-        String name = "Sanjay";
+        char letter = 'r';
     }
 }
