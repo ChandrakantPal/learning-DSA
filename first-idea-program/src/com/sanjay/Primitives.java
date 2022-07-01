2a6b9d655a4fec916106e25cdfd4c68f71d8d220
ChandrakantPal <sanjay161097@gmail.com>
2022-07-01 23:24:41 +0530

    float

--- first-idea-program/src/com/sanjay/Primitives.java
+++ first-idea-program/src/com/sanjay/Primitives.java
@@ -3,6 +3,7 @@ package com.sanjay;
 public class Primitives {
     public static void main(String[] args) {
         int rollno = 64;
-        char letter = 'r';
+        char letter = 's';
+        float marks = 96.54f;
     }
 }
