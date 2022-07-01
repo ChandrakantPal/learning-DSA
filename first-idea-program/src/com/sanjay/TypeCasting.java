a4371f6ec63502ce4e5e66fed2a5af6a4d288a8b
ChandrakantPal <sanjay161097@gmail.com>
2022-07-02 02:52:28 +0530

    automatic type promotion in expressions

--- first-idea-program/src/com/sanjay/TypeCasting.java
+++ first-idea-program/src/com/sanjay/TypeCasting.java
@@ -12,5 +12,18 @@ public class TypeCasting {
         int num = (int)(42.54f);
         System.out.println(num);
 
+        // automatic type promotion in expressions
+//        int a = 257;
+//        byte b = (byte) (a); //257 % 256 = 1
+
+//        byte a = 40;
+//        byte b  = 50;
+//        byte c = 100;
+//        int d = a*b/c; // a*b exceeds the byte can handle but while evaluating the a*b is converted to int
+//        System.out.println(d);
+
+//        byte b = 50;
+//        b = b*2;  this throws an error because while evaluating the expression it is converted to an int which cannot be reassigned to byte b
+
     }
 }
