23b8a58d2a493127fb679e42be05d7b65bae087e
ChandrakantPal <sanjay161097@gmail.com>
2022-07-02 02:31:29 +0530

    type casting

--- first-idea-program/src/com/sanjay/TypeCasting.java
+++ first-idea-program/src/com/sanjay/TypeCasting.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 public class TypeCasting {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        float num  = input.nextFloat();
+//        float num  = input.nextFloat();
+//        System.out.println(num);
+
+        //type casting
+        int num = (int)(42.54f);
         System.out.println(num);
+
     }
 }
