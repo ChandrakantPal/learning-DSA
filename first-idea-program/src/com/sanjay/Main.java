a2441955695c6911cfd104d817855d9b492aa973
ChandrakantPal <sanjay161097@gmail.com>
2022-07-01 23:08:57 +0530

    printing input

--- first-idea-program/src/com/sanjay/Main.java
+++ first-idea-program/src/com/sanjay/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println(input.nextInt());
         System.out.println("Hello world!");
         System.out.println("Hello Git");
     }
