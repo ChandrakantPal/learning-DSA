9ecec1e8fe4ac00429ae4d7096d9e636177ad890
ChandrakantPal <sanjay161097@gmail.com>
2022-07-02 02:28:45 +0530

    num

---
+++ first-idea-program/src/com/sanjay/TypeCasting.java
@@ -0,0 +1,11 @@
+package com.sanjay;
+
+import java.util.Scanner;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float num  = input.nextFloat();
+        System.out.println(num);
+    }
+}
