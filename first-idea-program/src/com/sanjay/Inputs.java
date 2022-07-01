ad56de6f8e19e013441988d1b1ea4a4ffc1d0063
ChandrakantPal <sanjay161097@gmail.com>
2022-07-02 02:06:36 +0530

    input.nextInt()

--- first-idea-program/src/com/sanjay/Inputs.java
+++ first-idea-program/src/com/sanjay/Inputs.java
@@ -6,5 +6,6 @@ public class Inputs {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter some input: ");
+        int rollno = input.nextInt();
     }
 }
