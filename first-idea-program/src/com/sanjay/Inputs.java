fa8fd5219649e06163418b6dbb15628e19a3d0e2
ChandrakantPal <sanjay161097@gmail.com>
2022-07-02 02:13:23 +0530

    string and float input

--- first-idea-program/src/com/sanjay/Inputs.java
+++ first-idea-program/src/com/sanjay/Inputs.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 public class Inputs {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter some input: ");
-        int rollno = input.nextInt();
-        System.out.println("Your roll number is " + rollno);
+//        System.out.println("Please enter some input: ");
+//        int rollno = input.nextInt();
+//        System.out.println("Your roll number is " + rollno);
+
+        String name = input.next();
+        System.out.println(name);
+
+        float marks = input.nextFloat();
     }
 }
