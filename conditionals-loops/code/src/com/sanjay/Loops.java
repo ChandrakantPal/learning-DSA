23f495113023e1327ce37e2fd59e42645645666d
ChandrakantPal <sanjay161097@gmail.com>
2022-07-03 23:25:15 +0530

    do while loop

--- conditionals-loops/code/src/com/sanjay/Loops.java
+++ conditionals-loops/code/src/com/sanjay/Loops.java
@@ -4,21 +4,27 @@ import java.util.Scanner;
 
 public class Loops {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        for (int num = 0; num < 5; num++) {
-            System.out.println(num);
-        }
+//        Scanner in = new Scanner(System.in);
+//        for (int num = 0; num < 5; num++) {
+//            System.out.println(num);
+//        }
+//
+//        int n = in.nextInt();
+//        for (int num = 1; num <= n; num++) {
+//            System.out.println("Hello World");
+//        }
+//
+//        int num = 1;
+//        while (num<=5){
+//            System.out.println(num);
+//            num++;
+//        }
 
-        int n = in.nextInt();
-        for (int num = 1; num <= n; num++) {
-            System.out.println("Hello World");
-        }
-
-        int num = 1;
-        while (num<=5){
-            System.out.println(num);
-            num++;
-        }
+        int n = 1;
+        do {
+            System.out.println(n);
+            n++;
+        }while (n<=5);
     }
 
 }
