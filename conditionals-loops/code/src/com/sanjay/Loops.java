ababd377b167f0cc09de8045b62f8d32cd934d62
ChandrakantPal <sanjay161097@gmail.com>
2022-07-03 23:16:29 +0530

    1 to n for loop

--- conditionals-loops/code/src/com/sanjay/Loops.java
+++ conditionals-loops/code/src/com/sanjay/Loops.java
@@ -4,8 +4,13 @@ import java.util.Scanner;
 
 public class Loops {
     public static void main(String[] args) {
-//        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         for (int num = 0; num < 5; num++) {
+            System.out.println(num);
+        }
+
+        int n = in.nextInt();
+        for (int num = 1; num <= n; num++) {
             System.out.println(num);
         }
     }
