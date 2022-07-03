05683af57afd1bcfc8c6991958df64828473ea2d
ChandrakantPal <sanjay161097@gmail.com>
2022-07-03 23:07:49 +0530

    basic for loop

---
+++ conditionals-loops/code/src/com/sanjay/Loops.java
@@ -0,0 +1,13 @@
+package com.sanjay;
+
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+        for (int num = 0; num < 5; num++) {
+            System.out.println(num);
+        }
+    }
+
+}
