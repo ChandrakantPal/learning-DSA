fd2fa9be4d91870369a372a600d743c70b70ef76
ChandrakantPal <sanjay161097@gmail.com>
2022-07-04 20:59:31 +0530

    Calculator function

--- conditionals-loops/code/src/com/sanjay/Calculator.java
+++ conditionals-loops/code/src/com/sanjay/Calculator.java
@@ -1,7 +1,41 @@
 package com.sanjay;
 
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int ans = 0;
+        while (true) {
+            // take the operator as input
+            char op = in.next().trim().charAt(0);
+            if (op == '+'||op == '-'||op == '/'||op == '%' || op == '*'){
+                // input two numbers
+                int num1 = in.nextInt();
+                int num2 = in.nextInt();
 
+                if (op == '+'){
+                    ans = num1 + num2;
+                }
+                if (op == '-'){
+                    ans = num1 - num2;
+                }
+                if (op == '*'){
+                    ans = num1 * num2;
+                }
+                if (op == '/'){
+                    if (num2 != 0) {
+                        ans = num1 / num2;
+                    }
+                }
+                if (op == '%'){
+                    ans = num1 % num2;
+                }
+
+            } else if (op == 'x' || op == 'X') {
+                break;
+            }
+        }
+        System.out.println(ans);
     }
 }
