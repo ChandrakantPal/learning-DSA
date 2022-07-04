734ce012a8976e64c798513c7e31ce3928a7b8fe
ChandrakantPal <sanjay161097@gmail.com>
2022-07-04 19:45:19 +0530

    CountNums

---
+++ conditionals-loops/code/src/com/sanjay/CountNums.java
@@ -0,0 +1,17 @@
+package com.sanjay;
+
+public class CountNums {
+    public static void main(String[] args) {
+       int n = 45536;
+        int count = 0;
+       while (n > 0){
+        int rem = n % 10;
+        if(rem == 5) {
+            count++;
+        }
+        n = n/10;
+       }
+       System.out.println(count);
+
+    }
+}
