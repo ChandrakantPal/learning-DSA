bdd024486673ca2a70af97a2b5ccbe4a34fb0653
ChandrakantPal <sanjay161097@gmail.com>
2022-07-05 21:24:57 +0530

    switch case

--- conditionals-loops/switch/src/com/chandrakant/Main.java
+++ conditionals-loops/switch/src/com/chandrakant/Main.java
@@ -1,7 +1,27 @@
 package com.chandrakant;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        String fruit = in.next();
+
+        switch (fruit) {
+            case "Mango":
+                System.out.println("King of fruits");
+                break;
+            case "Apple":
+                System.out.println("A sweet red fruit");
+                break;
+            case "Orange":
+                System.out.println("Round fruit");
+                break;
+            case "Grapes":
+                System.out.println("Small fruit");
+                break;
+            default:
+                System.out.println("please enter a valid fruit");
+        }
     }
 }
