e9da826698774c16b63430584ecc15144d5ec1ca
ChandrakantPal <sanjay161097@gmail.com>
2022-07-05 22:11:03 +0530

    Nested Switch

---
+++ conditionals-loops/switch/src/com/chandrakant/NestedSwitch.java
@@ -0,0 +1,32 @@
+package com.chandrakant;
+
+import java.util.Scanner;
+
+public class NestedSwitch {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int empID = in.nextInt();
+        String department = in.next();
+        switch (empID) {
+            case 1:
+                System.out.println("Chandrakant Pal");
+                break;
+            case 2:
+                System.out.println("Suresh Kapoor");
+                break;
+            case 3:
+                switch (department) {
+                    case "IT":
+                        System.out.println("IT Department");
+                        break;
+                    case "Management":
+                        System.out.println("Management Department");
+                        break;
+                    default:
+                        System.out.println("No department entered");
+                }
+            default:
+                System.out.println("Enter correct Employee ID");
+        }
+    }
+}
