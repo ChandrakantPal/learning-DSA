ddd39f4d2ce07b6447e1605805e9fe18f9fb7cd9
ChandrakantPal <sanjay161097@gmail.com>
2022-07-05 21:57:55 +0530

    switch case weekdays and weekend

--- conditionals-loops/switch/src/com/chandrakant/Main.java
+++ conditionals-loops/switch/src/com/chandrakant/Main.java
@@ -5,23 +5,37 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String fruit = in.next();
-
-        switch (fruit) {
-            case "Mango":
-                System.out.println("King of fruits");
+//        String fruit = in.next();
+//
+//        switch (fruit) {
+//            case "Mango":
+//                System.out.println("King of fruits");
+//                break;
+//            case "Apple":
+//                System.out.println("A sweet red fruit");
+//                break;
+//            case "Orange":
+//                System.out.println("Round fruit");
+//                break;
+//            case "Grapes":
+//                System.out.println("Small fruit");
+//                break;
+//            default:
+//                System.out.println("please enter a valid fruit");
+//        }
+        int day = in.nextInt();
+        switch (day){
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Weekday");
                 break;
-            case "Apple":
-                System.out.println("A sweet red fruit");
+            case 6:
+            case 7:
+                System.out.println("Weekend");
                 break;
-            case "Orange":
-                System.out.println("Round fruit");
-                break;
-            case "Grapes":
-                System.out.println("Small fruit");
-                break;
-            default:
-                System.out.println("please enter a valid fruit");
         }
     }
 }
