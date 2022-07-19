3e71b328cfba0d224e9ab35047eef80d1cefe180
Chandrakant Pal <sanjay161097@gmail.com>
2022-07-19 20:02:13 +0530

    return int sum

--- methods/src/com/chandrakant/Sum.java
+++ methods/src/com/chandrakant/Sum.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-    sum();
+//    sum();
+    int ans = sum2();
+        System.out.println(ans);
     }
 
     static void sum() {
@@ -17,4 +19,13 @@ public class Sum {
         System.out.println("The sum = "+ sum);
     }
 
+    static  int sum2(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter number 1: ");
+        int num1 = in.nextInt();
+        System.out.print("Enter number 2: ");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+        return  sum;
+    }
 }
