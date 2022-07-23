24229eda5f72b57c4a7dee2e83d56405fa001a55
Chandrakant Pal <sanjay161097@gmail.com>
2022-07-23 17:41:40 +0530

    main

--- linear-search/src/com/chandrakant/Main.java
+++ linear-search/src/com/chandrakant/Main.java
@@ -2,6 +2,25 @@ package com.chandrakant;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] nums = {23, 45, 1, 2, 8, 19, 0, 14, 55};
+        System.out.println(linearSearch2(nums, 0));
+    }
+
+    //search the target and return the element
+    static int linearSearch2(int[] arr, int target) {
+        if (arr.length == 0) return Integer.MAX_VALUE;
+        for (int j : arr) {
+            if (j == target) return j;
+        }
+        return Integer.MAX_VALUE;
+    }
+
+    //search the array return index if item found else return -1
+    static int linearSearch(int[] arr, int target) {
+        if (arr.length == 0) return -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) return i;
+        }
+        return -1;
     }
 }
