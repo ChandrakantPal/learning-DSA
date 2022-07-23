e24638a01ae368102c205acfd305f4546a20e876
Chandrakant Pal <sanjay161097@gmail.com>
2022-07-23 17:47:52 +0530

    string search

---
+++ linear-search/src/com/chandrakant/SearchInString.java
@@ -0,0 +1,17 @@
+package com.chandrakant;
+
+public class SearchInString {
+    public static void main(String[] args) {
+        String name = "Chandrakant";
+        char target = 'r';
+        System.out.println(search(name, target));
+    }
+
+    static boolean search(String str, char target) {
+        if (str.length() == 0) return false;
+        for (int i = 0; i < str.length(); i++) {
+            if (target == str.charAt(i)) return true;
+        }
+        return false;
+    }
+}
