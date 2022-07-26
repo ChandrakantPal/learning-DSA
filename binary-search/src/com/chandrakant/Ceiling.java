e5f360b389953346a7beb813fe143a775959520f
Chandrakant Pal <sanjay161097@gmail.com>
2022-07-26 23:14:09 +0530

    ceiling

---
+++ binary-search/src/com/chandrakant/Ceiling.java
@@ -0,0 +1,35 @@
+package com.chandrakant;
+
+public class Ceiling {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 5, 7, 9, 12, 16, 19, 25, 36, 48};
+        int target = 15;
+        int ans = ceiling(arr, target);
+        System.out.println(arr[ans]);
+    }
+
+    //return the index of the smallest no >= target
+    static int ceiling(int[] arr, int target) {
+
+        // but what if the target is greater than the greatest number in the array
+        if (target > arr[arr.length - 1]) return -1;
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            // find the middle element
+            // int mid = (start + end ) / 2; // might be possible that (start + end ) exceeds the range of int
+            int mid = start + (end - start) / 2; // (2s + e -s)/2=(s+e)/2
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                // answer
+                return mid;
+            }
+        }
+        return start;
+    }
+}
