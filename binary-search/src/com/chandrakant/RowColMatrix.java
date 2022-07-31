6c08b68b13eb706e14a79f880f13dd3cd87374b3
Chandrakant Pal <sanjay161097@gmail.com>
2022-07-31 19:29:54 +0530

    print statement removed

--- binary-search/src/com/chandrakant/RowColMatrix.java
+++ binary-search/src/com/chandrakant/RowColMatrix.java
@@ -4,14 +4,13 @@ import java.util.Arrays;
 
 public class RowColMatrix {
     public static void main(String[] args) {
-        System.out.println("Binary Search in 2D Array");
         int[][] arr = {
                 {10, 20, 30, 40},
                 {15, 25, 35, 45},
                 {28, 29, 37, 49},
                 {33, 34, 38, 50}
         };
-        System.out.println(Arrays.toString(search(arr, 49)));
+        System.out.println(Arrays.toString(search(arr, 37)));
     }
 
     static int[] search(int[][] matrix, int target) {
