81cb4d14fefcbe5140abb83fdcf36a85c4a62652
Chandrakant Pal <sanjay161097@gmail.com>
2022-08-03 16:13:27 +0530

    insertion sort

--- sorting/src/com/chandrakant/Main.java
+++ sorting/src/com/chandrakant/Main.java
@@ -9,6 +9,19 @@ public class Main {
         System.out.println(Arrays.toString(arr));
     }
 
+    static void insertion(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) { // <arr.length -1 is same as <=arr.length-2
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                } else {
+                    break;
+                }
+
+            }
+        }
+    }
+
     static void selection(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             // find the max item in the remaining array and swap with correct index
