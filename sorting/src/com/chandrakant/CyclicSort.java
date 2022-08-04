d9910444e58e89d3e2433e6f01f4ae05145f6b60
Chandrakant Pal <sanjay161097@gmail.com>
2022-08-04 18:14:57 +0530

    CyclicSort

---
+++ sorting/src/com/chandrakant/CyclicSort.java
@@ -0,0 +1,29 @@
+package com.chandrakant;
+
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 2, 1, 4};
+        cyclic(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void cyclic(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correctIndex = arr[i] - 1;
+            if (arr[i] != arr[correctIndex]) {
+                swap(arr, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
