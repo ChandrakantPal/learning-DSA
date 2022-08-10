06d64796b9cfacdd1b862d1b5bfe419eecaf49a9
Chandrakant Pal <sanjay161097@gmail.com>
2022-08-10 23:53:14 +0530

    addToArrayForm method

--- leetcode-practice/src/com/chandrakant/ArrayFormOfInteger.java
+++ leetcode-practice/src/com/chandrakant/ArrayFormOfInteger.java
@@ -4,4 +4,12 @@ public class ArrayFormOfInteger {
     public static void main(String[] args) {
 
     }
+
+    static int addToArrayForm(int[] num, int k) {
+        int number = 0;
+        for (int i = num.length - 1; i <= 0; i--) {
+            number += Math.pow(num[i], i - num.length - 1);
+        }
+        return number + k;
+    }
 }
