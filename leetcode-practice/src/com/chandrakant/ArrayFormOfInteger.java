c3f442ee2f6fd5db726ded1e373d071f212b0a54
Chandrakant Pal <sanjay161097@gmail.com>
2022-08-11 00:24:45 +0530

    numberToArray method

--- leetcode-practice/src/com/chandrakant/ArrayFormOfInteger.java
+++ leetcode-practice/src/com/chandrakant/ArrayFormOfInteger.java
@@ -1,15 +1,27 @@
 package com.chandrakant;
 
+import java.util.ArrayList;
+
 public class ArrayFormOfInteger {
     public static void main(String[] args) {
-
+        int[] arr = {1, 2, 0, 0};
+        System.out.println(addToArrayForm(arr, 34));
     }
 
-    static int addToArrayForm(int[] num, int k) {
+    static ArrayList<Integer> addToArrayForm(int[] num, int k) {
         int number = 0;
-        for (int i = num.length - 1; i <= 0; i--) {
-            number += Math.pow(num[i], i - num.length - 1);
+        for (int i = 0; i < num.length; i++) {
+            number += num[num.length - 1 - i] * Math.pow(10, i);
         }
-        return number + k;
+        return numberToArray(number + k);
+    }
+
+    static ArrayList<Integer> numberToArray(int num) {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        while (num > 0) {
+            arr.add(num % 10);
+            num /= 10;
+        }
+        return arr;
     }
 }
